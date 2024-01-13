62a364479042fb28ea33a2bd1cf41ac8d3f60f83
Stuart Bate <bate.stuart93@gmail.com>
2024-01-13 12:57:17 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+//fetch from the Server
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        //post request for name and email address
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      //appends id to the DOM
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      //handles failed post
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
